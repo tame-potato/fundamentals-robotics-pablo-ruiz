48fe3aa38b00de56120731768702b326e49bffa3
Pablo Ruiz <pablo_ruiz@student.uml.edu>
2020-12-25 01:29:01 -0500

    Lab 5

--- packages/lab4/src/lab4_node.py
+++ packages/lab4/src/lab4_node.py
@@ -4,50 +4,96 @@ import rospy
 import math
 import message_filters
 from duckietown_msgs.msg import WheelEncoderStamped
-from lab4.msg import Pose2D
+from duckietown_msgs.msg import Twist2DStamped
+from lab4.msg import Pose2D_dist
 
 class odometry:
     def __init__(self):
 
         self.sub_left = message_filters.Subscriber('left_wheel_encoder_node/tick', WheelEncoderStamped)
         self.sub_right = message_filters.Subscriber('right_wheel_encoder_node/tick', WheelEncoderStamped)
+        self.sub = rospy.Subscriber('car_cmd_switch_node/cmd', Twist2DStamped, self.velocity_direction )
+
         self.ts = message_filters.TimeSynchronizer([self.sub_left, self.sub_right], queue_size=10)
         self.ts.registerCallback(self.callback)
 
-        self.pub = rospy.Publisher('current_pose', Pose2D, queue_size=10)
+        self.pub = rospy.Publisher('current_pose', Pose2D_dist, queue_size=10)
 
-        self.position = Pose2D()
-        self.position.x = 0
-        self.position.y = 0
-        self.position.theta = 0
+        self.pose = Pose2D_dist()
+        self.pose.x = 0
+        self.pose.y = 0
+        self.pose.theta = 0
+        self.pose.distance_travelled = 0
+        self.previous_l_ticks = 0
+        self.previous_r_ticks = 0
+        self.sign_left = 1
+        self.sign_right = 1
 
-        self.distance = 0.1
+        self.axleSpacing = 0.1
         self.diameter = 0.068
         self.ticks_total = 135
 
     def delta(self,delta_r, delta_l):
         delta_s = (delta_r+delta_l) / 2
-        delta_theta = (delta_r-delta_l) / (2*self.distance)
+        delta_theta = (delta_r-delta_l) / (2*self.axleSpacing)
         return delta_s, delta_theta
 
     def ticks_to_m(self, ticks):
         return self.diameter * math.pi / self.ticks_total * ticks
 
-    def callback(self, left_tick, right_tick):
+    def velocity_direction(self, twist):
 
-        delta_r =  self.ticks_to_m(right_tick.data)
-        delta_l =  self.ticks_to_m(left_tick.data)
+        if twist.v > 0:
 
-        delta_s, delta_theta = self.delta(delta_r,delta_l)
+            self.sign_right = 1
+            self.sign_left = 1
 
-        self.position.x += delta_s*math.cos(self.position.theta + (delta_theta/2))
-        self.position.y += delta_s*math.sin(self.position.theta + (delta_theta/2))
-        self.position.theta += delta_theta
+        elif twist.v < 0:
 
-        rospy.loginfo(self.position)
-        self.pub.publish(self.position)
+            self.sign_right = -1
+            self.sign_left = -1
 
-    
+        elif twist.omega > 0:
+
+            self.sign_right = 1 
+            self.sign_left = -1
+
+        else:
+
+            self.sign_right = -1 
+            self.sign_left = 1
+
+    def callback(self, full_left_tick, full_right_tick):
+
+        rospy.logwarn("Right: "+str(full_right_tick.data))
+        rospy.logwarn("Left: "+str(full_left_tick.data))
+
+        if self.previous_l_ticks == 0 or self.previous_r_ticks == 0:
+
+            self.previous_l_ticks = full_left_tick.data
+            self.previous_r_ticks = full_right_tick.data
+
+        else:
+
+            left_tick = self.sign_left*(full_left_tick.data - self.previous_l_ticks)
+            right_tick = self.sign_right*(full_right_tick.data - self.previous_r_ticks)
+
+            delta_r =  self.ticks_to_m(right_tick)
+            delta_l =  self.ticks_to_m(left_tick)
+
+            delta_s, delta_theta = self.delta(delta_r,delta_l)
+
+            self.pose.x += delta_s*math.cos(self.pose.theta + (delta_theta/2))
+            self.pose.y += delta_s*math.sin(self.pose.theta + (delta_theta/2))
+            self.pose.theta += delta_theta
+            #self.pose.theta = (self.pose.theta / 2*math.pi) % 1 * (2*math.pi)
+
+            self.pub.publish(self.pose)
+
+            self.pose.distance_travelled += delta_s 
+            self.previous_l_ticks = full_left_tick.data
+            self.previous_r_ticks = full_right_tick.data
+        
 if __name__ == '__main__':
 
     rospy.init_node('odometry')
--- packages/lab5/src/lab5_node.py
+++ packages/lab5/src/lab5_node.py
@@ -2,10 +2,11 @@
 
 import rospy
 import cv2
-import system
+import sys
 import numpy as np
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge
+from duckietown_msgs.msg import SegmentList, Segment
 
 class process_image():
 
@@ -15,62 +16,127 @@ class process_image():
 
         self.sub = rospy.Subscriber("camera_node/image/compressed", CompressedImage, self.callback, queue_size=1, buff_size=2**24)
 
-        self.image_size = (160,120)
+        self.img_size = (160, 120)
         self.offset = 40
 
-        self.pub_cropped = rospy.Publisher( "image_cropped", Image , queue_size=10)
-        self.kernel_errode = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+        self.pub = rospy.Publisher( "line_detector_node/segment_list", SegmentList , queue_size=10)
+        self.pub_debug_img = rospy.Publisher("lab5_node/debug_img", Image, queue_size=1)
+        self.pub_white = rospy.Publisher("lab5_node/debug_img_white", Image, queue_size=1)
+        self.pub_yellow = rospy.Publisher("lab5_node/debug_img_yellow", Image, queue_size=1)
+
+        #self.kernel_errode = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
         self.kernel_dilate = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
+
+    def plotLines(self, img, lines, color=(255,0,0)):
+
+        for i in lines:
+            cv2.line(img, (i[0][0],i[0][1]), (i[0][2],i[0][3]), color, 2)
+
+        return img
+
+
+    def to_segment_list(self, lines_white_norm, lines_yellow_norm):
+
+        segment_list = []
+
+        if len(lines_white_norm) > 0:
+            for x1, y1, x2, y2 in lines_white_norm: 
+
+                segment = Segment()
+                segment.color = 0
+
+                segment.pixels_normalized[0].x = x1
+                segment.pixels_normalized[0].y = y1
+                segment.pixels_normalized[1].x = x2
+                segment.pixels_normalized[1].y = y2
+
+                segment_list.append(segment)
+
+
+        if len(lines_yellow_norm) > 0:
+            for x1, y1, x2, y2 in lines_yellow_norm: 
+
+                segment = Segment()
+                segment.color = 0
+
+                segment.pixels_normalized[0].x = x1
+                segment.pixels_normalized[0].y = y1
+                segment.pixels_normalized[1].x = x2
+                segment.pixels_normalized[1].y = y2
+
+                segment_list.append(segment)
+
+
+        return segment_list
+
 
     def callback(self, rosImage):
 
+        segment_list = SegmentList()
+
         cvImage = self.bridge.compressed_imgmsg_to_cv2(rosImage)
-        cvImage = cv2.resize(cvImage, self.image_size, interpolation=cv2.INTER_NEAREST)
-        cvImage = new_image[self.offset:,:]
+        cvImage = cv2.resize(cvImage, self.img_size, interpolation=cv2.INTER_NEAREST)
+        cvImage = cvImage[self.offset:,:]
         cvImage_HSV = cv2.cvtColor(cvImage, cv2.COLOR_BGR2HSV)
 
-        cvImage_filtered_white = cv2.inRange(cvImage_HSV, (0,0,150), (255,30,255))
-        cvImage_filtered_yellow = cv2.inRange(cvImage_HSV, (30,100,150), (60,255,255))
+        arr_cutoff = np.array([0, self.offset, 0, self.offset])
+        arr_ratio = np.array([1. / self.img_size[1], 1. / self.img_size[0], 1. / self.img_size[1], 1. / self.img_size[0]])
 
-        cvImage_filtered_white = cv2.erode(cvImage_filtered_white, self.kernel_errode)
-        cvImage_filtered_yellow = cv2.erode(cvImage_filtered_yellow, self.kernel_errode)
+        cvImage_filtered_white = cv2.inRange(cvImage_HSV, (35,0,100), (179,60,255))
+        cvImage_filtered_yellow = cv2.inRange(cvImage_HSV, (29,100,100), (35,255,255))
+
+        #cvImage_filtered_white = cv2.erode(cvImage_filtered_white, self.kernel_errode)
+        #cvImage_filtered_yellow = cv2.erode(cvImage_filtered_yellow, self.kernel_errode)
         cvImage_filtered_white = cv2.dilate(cvImage_filtered_white, self.kernel_dilate)
         cvImage_filtered_yellow = cv2.dilate(cvImage_filtered_yellow, self.kernel_dilate)
-
-        img_cropped_white = cvImage.copy()
-        img_cropped_yellow = cvImage.copy()
 
         img_edges = cv2.Canny(cvImage, 100, 400)
 
         img_edges_white = cv2.bitwise_and(img_edges, img_edges, mask=cvImage_filtered_white)
         img_edges_yellow = cv2.bitwise_and(img_edges,img_edges, mask=cvImage_filtered_yellow)
 
-        lines_white = cv2.HoughLinesP(img_edges_white, rho=1, theta=np.pi/180, threshold=5, minLineLe
-ngth=1, maxLineGap=1)
-        lines_yellow = cv2.HoughLinesP(img_edges_yellow, rho=1, theta=np.pi/180, threshold=5, minLine
-Length=1, maxLineGap=1)
+        lines_white = cv2.HoughLinesP(img_edges_white, rho=1, theta=np.pi/180, threshold=5, minLineLength=1, maxLineGap=1)
+        lines_yellow = cv2.HoughLinesP(img_edges_yellow, rho=1, theta=np.pi/180, threshold=5, minLineLength=1, maxLineGap=1)
+     
+        if lines_white is not None:
 
-        for i in lines_white:
-            cv2.line(cvImage, (i[0][0],i[0][1]), (i[0][2],i[0][3]), (255,0,0), 2)
-            cv2.line(img_cropped_white, (i[0][0],i[0][1]), (i[0][2],i[0][3]), (255,0,0), 2)
+            cvImage = self.plotLines(cvImage, lines_white)
 
-        for i in lines_yellow:
-            cv2.line(cvImage, (i[0][0],i[0][1]), (i[0][2],i[0][3]), (0,0,255), 2)
-            cv2.line(img_cropped_yellow, (i[0][0],i[0][1]), (i[0][2],i[0][3]), (0,0,255), 2)
+            lines_white = lines_white.reshape((-1, 4))
+            lines_white_norm = np.multiply(np.add(lines_white,arr_cutoff), arr_ratio)
 
-        cvImage = self.bridge.cv2_to_imgmsg(img_cropped_all, "bgr8")
-        img_cropped_white = self.bridge.cv2_to_imgmsg(img_cropped_white, "bgr8")
-        img_cropped_yellow = self.bridge.cv2_to_imgmsg(img_cropped_yellow, "bgr8")
+        else:
+            lines_white_norm = []
 
-        self.pub.publish(img_cropped_all)
-        self.pub_white.publish(img_cropped_white)
-        self.pub_yellow.publish(img_cropped_yellow)
+        if lines_yellow is not None:
+
+            cvImage = self.plotLines(cvImage, lines_yellow, color=(0,0,255))
+
+            lines_yellow = lines_yellow.reshape((-1, 4))
+            lines_yellow_norm = np.multiply(np.add(lines_yellow,arr_cutoff), arr_ratio)
+        
+        else:
+            lines_yellow_norm = []
+
+        segment_list.segments.extend(self.to_segment_list(lines_white_norm, lines_yellow_norm))
+        
+        cvImage = self.bridge.cv2_to_imgmsg(cvImage, "bgr8")
+
+        cvImage_filtered_white = self.bridge.cv2_to_imgmsg(cvImage_filtered_white, "mono8")
+        cvImage_filtered_yellow = self.bridge.cv2_to_imgmsg(cvImage_filtered_yellow, "mono8")
+
+        self.pub.publish(segment_list)
+        self.pub_debug_img.publish(cvImage)
+        self.pub_white.publish(cvImage_filtered_white)
+        self.pub_yellow.publish(cvImage_filtered_yellow)
+
+        rospy.logwarn("PABLO'S LAB 5 CODE")
 
 if __name__ == "__main__":
 
     rospy.init_node("edge_detection")
 
-    detect_edges()
+    process_image()
 
     rospy.spin()
 
